2bb4a6883bafdcdc0b8a28c40243b8a6daf76fd3
株式会社ミエフソー <miefuso@mac2025.local>
2026-03-16 15:27:38 +0900

    add GitHub Pages deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import { fileURLToPath } from 'url';
 
 export default defineConfig({
+  site: 'https://miefuso-web-admin.github.io',
+  base: '/web',
   vite: {
     resolve: {
       alias: {
